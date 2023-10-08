0805fd0dafccc884f5a7fd8d1422c044771dd962
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-10-08 20:53:20 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+import SortingVisualizer from "./components/SortingVisualizer.tsx";
+function App(){
+    return (
+        <>
+            <SortingVisualizer/>
+        </>
+  )
+}
+
+export default App
